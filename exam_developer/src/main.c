37a62f6b6191a5b8400c70e57271c71f3c9b65ee
MaxiL994 <liebl.maximilian@gmail.com>
2025-03-17 20:44:14 +0100

    Main.c v1.1 finalization
    Added switch case function to loop switching between options. Changed directory of header file

--- exam_developer/src/main.c
+++ exam_developer/src/main.c
@@ -1,4 +1,4 @@
-#include "developer_group.h"
+#include "../include/developer_group.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,6 +13,9 @@ int main(void) {
     /* Initialize developer group */
     developer_group_t group = developer_group_init(devs, 2, GROUP_LOGO);
 
+    /* 
+    Print menu options for user to choose from.
+    */
     int choice;
     do {
 
@@ -26,9 +29,45 @@ int main(void) {
         printf("==============================\n");
         printf("\n");
         printf("Enter your choice: ");
-        
+        #
+
+        /*
+        Check if input is an integer between 1 and 4, else display error message
+        */
         if (scanf("%d", &choice) != 1) {
             printf("Error: Invalid input\n");
             while (getchar() != '\n'); /* Clear input buffer */
             continue;
         }
+
+/*
+Switch case for menu options. 
+1. List Developers
+2. Print Group Logo
+3. Print Group
+4. Exit
+Runs until user presses 4 to exit.
+*/
+
+        switch (choice) {
+            case 1:
+                developer_group_list_developers(&group);
+                break;
+            case 2:
+                developer_group_print_logo(&group);
+                break;
+            case 3:
+                developer_group_print(&group);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Error: Invalid choice\n");
+        }
+    } while (choice != 4);
+
+    /* Free allocated memory */
+    developer_group_free(&group);
+    return 0;
+}
