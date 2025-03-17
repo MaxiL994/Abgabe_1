23cded6e3afc9831786a8e2efbee94c587919853
MaxiL994 <liebl.maximilian@gmail.com>
2025-03-17 21:10:02 +0100

    Final project
    Code is working, logo slightly off when displayed

---
+++ exam_developer/build/CMakeFiles/CacheCows_exec.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CacheCows_exec.dir/link.d"
+  "CMakeFiles/CacheCows_exec.dir/src/main.c.o"
+  "CMakeFiles/CacheCows_exec.dir/src/main.c.o.d"
+  "CacheCows_exec"
+  "CacheCows_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CacheCows_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exam_developer/build/CMakeFiles/devlib.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for devlib.
+# This may be replaced when dependencies are built.
