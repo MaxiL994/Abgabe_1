4e45058fe0a7fd22fe26da14c3fa2c0372dc0e0c
marcbgc <begicm@outlook.de>
2025-03-17 12:18:10 +0100

    added comments
    added comments for if else function in developer.c

--- exam_developer/src/developer.c
+++ exam_developer/src/developer.c
@@ -10,7 +10,11 @@ developer_t developer_init(const char *name, const char *alias) {
     return dev;
 }
 
-/* Print developer details to the console */
+/* 
+Print developer details to the console
+checking if name and alias is entered in developer.h
+else print error message
+*/
 void developer_print(const developer_t *dev) {
     if (dev && dev->name && dev->alias) {
         printf("Name: %s, Alias: %s\n", dev->name, dev->alias);
